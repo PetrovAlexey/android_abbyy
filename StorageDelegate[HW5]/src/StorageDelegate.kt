9d43d34fe3402bcb7e853983c171b3e3b0a48219
Alexey <petrovalex_99@mail.ru>
2019-12-08 12:39:48 +0300

    HW5

---
+++ StorageDelegate[HW5]/src/StorageDelegate.kt
@@ -0,0 +1,41 @@
+package main
+
+import java.io.Serializable
+import kotlin.reflect.KProperty
+
+class StorageDelegate<T> constructor(private val key: String, value: T) {
+
+    private val storage : Storage = Storage()
+
+    private fun putValue(value: T) {
+        when (value) {
+            is String -> {
+                storage.put(key, value as String)
+            }
+            is Char -> {
+                storage.put(key, value as Char)
+            }
+            is Int -> {
+                storage.put(key, value as Int)
+            }
+            is Long -> {
+                storage.put(key, value as Long)
+            }
+            is Double -> {
+                storage.put(key, value as Double)
+            }
+            else -> {
+                throw IllegalArgumentException()
+            }
+        }
+    }
+
+    init {
+        putValue(value)
+    }
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T = storage.get(key) as T
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) = putValue(value)
+
+}
